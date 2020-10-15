fe9ecb574b2df59d4fcfbca42fc6cd37b16fe67f
Jelena Jovanovic <jeljov@gmail.com>
2020-10-15 14:29:59 +0200

    Completed tasks after Lab 1

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -2,7 +2,17 @@
 # Write a function that asks the user for a number, and depending on whether
 # the number is even or odd, prints out an appropriate message.
 
+def odd_or_even():
+    number_str = input("Please enter a whole number\n")
 
+    # Option 1:
+    # if int(number_str) % 2 == 0:
+    #     print("EVEN number")
+    # else:
+    #     print("ODD number")
+
+    result = "EVEN" if int(number_str) % 2 == 0 else "ODD"
+    print("Number", number_str, "is", result)
 
 
 # Task 2
@@ -11,7 +21,11 @@
 # as an argument. The computed factorial value should be
 # printed to the console.
 
-
+def factorial(number):
+    result = 1
+    for i in range(1, number+1):
+        result *= i
+    print("Factorial of number", number, "is", result)
 
 
 # Task 3
@@ -20,7 +34,10 @@
 # value if n (2nd input parameter) is greater than the number of
 # elements in the iterable.
 
-
+def nth_lowest(iterable, n):
+    if len(iterable) < n:
+        return min(iterable)
+    return sorted(iterable)[n-1]
 
 
 # Task 4
@@ -31,7 +48,16 @@
 # - the sum of all positive elements in the list
 # - the product of all negative elements in the list
 
+def list_stats(numbers):
+    abs_numbers = list()
+    sum_pos = 0
+    prod_neg = 1
+    for number in numbers:
+        abs_numbers.append(abs(number))
+        if number > 0: sum_pos += number
+        elif number < 0: prod_neg *= number
 
+    return min(abs_numbers), max(abs_numbers), sum_pos, prod_neg
 
 
 
@@ -44,7 +70,16 @@
 # - sorts the elements in the new list in the descending order,
 #   and prints them, one element per line
 
+def list_operations(numbers, threshold):
+    unique_nums = list()
+    for num in set(numbers):
+        if num < threshold: unique_nums.append(num)
 
+    print("The new list has", len(unique_nums), "elements")
+
+    unique_nums.sort(reverse=True)
+    for i, num in enumerate(unique_nums):
+        print(str(i+1) + ". " + str(num))
 
 
 
@@ -55,7 +90,18 @@
 # letters of a different word or phrase, typically using all the
 # original letters exactly once
 
+def anagrams(s1, s2):
+    s1_list = list()
+    s2_list = list()
+    for ch in s1:
+        if ch.isalpha(): s1_list.append(ch.lower())
+    for ch in s2:
+        if ch.isalpha(): s2_list.append(ch.lower())
 
+    # s1_list.sort()
+    # s2_list.sort()
+    # return s1_list == s2_list
+    return sorted(s1_list) == sorted(s2_list)
 
 
 
@@ -65,7 +111,12 @@
 # Note: a palindrome is a word, phrase, or sequence that reads the same
 # backwards as forwards, e.g. "madam" or "nurses run".
 
+def palindrome(string):
+    alpha_num = list()
+    for ch in string:
+        if ch.isalnum(): alpha_num.append(ch.lower())
 
+    return alpha_num == list(reversed(alpha_num))
 
 
 
@@ -83,22 +134,39 @@
 # Hint: use function randint from random package to generate a number to
 # be guessed in the game
 
+def guessing_game():
+    from random import randint
+    hidden_number = randint(1,9)
+
+    trial = 1
+    while(trial <= 3):
+        trial += 1
+        guess = input("Make a guess (number between 1 and 9)\n")
+        if (len(guess)) > 1 or (guess.isdigit() == False):
+            print("Only digits between 1 and 9 are allowed. Try again")
+            continue
+        if int(guess) == hidden_number:
+            print("Well guessed! Congrats!")
+            return
+        else:
+            print("Wrong! Try again")
+
+    print("No more trials left. More luck next time")
+
 
 
 
 
 if __name__ == '__main__':
 
-    pass
-
     # odd_or_even()
 
-    # factorial(7)
+    # factorial(9)
 
     # a = [31, 72, 13, 41, 5, 16, 87, 98, 9]
-    # print(nth_lowest_v2(a, 3))
-    # print(nth_lowest_v2(['f', 'r', 't', 'a', 'b', 'y', 'j', 'd', 'c'], 6))
-    # print(nth_lowest_v2('today', 6))
+    # print(nth_lowest(a, 3))
+    # print(nth_lowest(['f', 'r', 't', 'a', 'b', 'y', 'j', 'd', 'c'], 6))
+    # print(nth_lowest('today', 2))
 
     # print(list_stats([1.2, 3.4, 5.6, -4.2, -5.6, 9, 11.3, -23.45, 81]))
 
@@ -106,10 +174,11 @@ if __name__ == '__main__':
 
     # print(anagrams('School master', 'The classroom'))
     # print(anagrams('Dormitory', 'Dirty room'))
+    # print(anagrams('Conversation', 'Voices rant on'))
     # print(anagrams('Bob', 'Bill'))
 
     # print(palindrome("madam"))
     # print(palindrome("nurses run"))
     # print(palindrome("nurse run"))
 
-    # guess_number()
+    guessing_game()
