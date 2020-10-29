e3b04d2409c5ba27faa51f480a63f2e3d3e04674
Jelena Jovanovic <jeljov@gmail.com>
2020-10-29 14:08:18 +0100

    Completed tasks for Lab 3

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -1,3 +1,6 @@
+from operator import itemgetter
+
+
 # Task 1
 # Write a function that creates a dictionary from the two given lists.
 # Assure the lists are of equal length.
@@ -8,6 +11,22 @@
 # should be turned into a dictionary where keys are country names and values
 # are the corresponding dishes.
 
+def lists_to_dict(l1, l2):
+
+    if len(l1) != len(l2):
+        print(f"Lists are not of equal size; the first {min(len(l1), len(l2))} elements of both lists will be used")
+
+    # Option 1
+    # d = dict(zip(l1, l2))
+    # for key, val in sorted(d.items(), key=itemgetter(1)):
+    #     print(f"{key}: {val}")
+
+    # Option 2
+    elem_cnt = min(len(l1), len(l2))
+    d = {l1[i]:l2[i] for i in range(elem_cnt)}
+
+    for key, val in sorted(d.items(), key=itemgetter(1)):
+        print(f"{key}: {val}")
 
 
 
@@ -27,6 +46,25 @@
 # - before being printed, the dictionary entries are sorted i) in the decreasing order
 #   of the tokens' frequencies, and then ii) in increasing alphabetical order.
 
+def token_frequency(text):
+
+    # Option 1
+    from collections import defaultdict
+    token_freq_dict = defaultdict(int)
+    for token in text.split():
+        token = token.rstrip(',.;:!?').lstrip()
+        if len(token) > 2:
+            token_freq_dict[token.lower()] += 1
+
+    for token, freq in sorted(sorted(token_freq_dict.items()), key=itemgetter(1), reverse=True):
+        print(f"{token}: {freq}")
+
+    # Option 2
+    # from collections import Counter
+    # tokens = [token.strip(',.;:!?').lower() for token in text.split()]
+    # tokens_counter = Counter([t for t in tokens if len(t) > 2])
+    # for token, freq in sorted(sorted(tokens_counter.items()), key=itemgetter(1), reverse=True):
+    #     print(f"{token}: {freq}")
 
 
 
@@ -42,6 +80,35 @@
 # Passwords that match the criteria should be printed in one line
 # separated by a comma.
 
+def password_check(passwords):
+
+    # Option 1
+    # from string import ascii_lowercase, ascii_uppercase, digits
+    # valid_passwords = []
+    # for pword in [p.strip() for p in passwords.split(",")]:
+    #     if any([ch in ascii_lowercase for ch in pword]) and \
+    #         any([ch in ascii_uppercase for ch in pword]) and \
+    #         any([ch.isdigit() for ch in pword]) and \
+    #         any([ch in '$#@' for ch in pword]) and \
+    #         6 <= len(pword) <= 12: valid_passwords.append(pword)
+    #
+    # print(", ".join(valid_passwords))
+
+    # Option 2
+    valid_passwords = list()
+
+    to_check = [p.strip() for p in passwords.split(',')]
+    for pword in to_check:
+        valid = [False] * 5
+        if any([(ch.isalpha() and ch.islower()) for ch in pword]): valid[0] = True
+        if any([ch.isdigit() for ch in pword]): valid[1] = True
+        if any([(ch.isalpha() and ch.isupper()) for ch in pword]): valid[2] = True
+        if any([ch in '$#@' for ch in pword]): valid[3] = True
+        if 6 <= len(pword) <= 12: valid[4] = True
+        if all(valid):
+            valid_passwords.append(pword)
+
+    print(", ".join(valid_passwords))
 
 
 
@@ -56,6 +123,37 @@
 # The function prints this list sorted by the members' scores (from highest to lowest) and
 # then returns the list as its return value.
 
+# def get_member_score(member_data):
+#     return member_data['score']
+
+get_member_score = lambda member_data: member_data['score']
+
+def collect_team_data():
+
+    print("""
+    You'll be prompted to enter name, age, and score for each team member.
+    Each member's data should be entered in a single line, separated by a comma.
+    To terminate the data entry enter "done".
+    """)
+
+    members = list()
+    while(True):
+        entry = input("Data for the next member:\n")
+        if entry.lower() == 'done': break
+        member_data = entry.split(",")
+        if len(member_data) != 3:
+            print("Irregular entry; please try again")
+            continue
+        name, age, score = member_data
+        members.append({'name': name, 'age': int(age), 'score': float(score)})
+
+    # alternative
+    # for member in sorted(members, key=itemgetter('score'), reverse=True):
+    for member in sorted(members, key=get_member_score, reverse=True):
+        name, age, score = member.values()
+        print(f"{name}, {age} years old, scored {score}")
+
+    return members
 
 
 
@@ -68,6 +166,29 @@
 #
 # Hint: the 'statistics' module provides functions for the required computations
 
+def team_stats(team_members):
+    from statistics import mean, quantiles
+
+    mean_age = mean([member['age'] for member in team_members])
+    print(f"Team members are {mean_age} years old, on average")
+
+    scores = [member['score'] for member in team_members]
+    q1, q2, q3 = quantiles(scores, n=4)
+    print(f"Team's median score is {q2}, whereas the interquartile range is [{q1:.2f}, {q3:.2f}]")
+
+    # Option 1 for max_player
+    # max_player = team_members[0]
+    # for player in [member for member in team_members if member['age'] < 21]:
+    #     if player['score'] > max_player['score']:
+    #         max_player = player
+
+    # Option 2 for max_player
+    # max_player = max([member for member in team_members if member['age'] < 21], key=get_member_score)
+
+    # Option 2.1 for max_player
+    max_player = max([member for member in team_members if member['age'] < 21], key=itemgetter('score'))
+
+    print(f"{max_player['name']} is the best player among those under 21 years of age")
 
 
 
@@ -81,10 +202,29 @@
 # After testing the function, try writing it using the Counter class from
 # the collections module.
 
+def classroom_stats(class_data):
+    from collections import Counter
+    # from collections import defaultdict
+    #
+    # classes_dict = defaultdict(int)
+    # for cls_label, cls_count in class_data:
+    #     classes_dict[cls_label] += cls_count
+    #
+    # for cls_label, cls_size in sorted(classes_dict.items(), key=itemgetter(1), reverse=True):
+    #     print(f"Class {cls_label} has {cls_size} students")
+
+    classes_list = []
+    for cls_label, cls_count in class_data:
+        classes_list.extend([cls_label] * cls_count)
+
+    classes_counts = Counter(classes_list)
+    for cls_label, cls_size in sorted(classes_counts.items(), key=itemgetter(1), reverse=True):
+        print(f"Class {cls_label} has {cls_size} students")
 
 
 
 if __name__ == '__main__':
+
     pass
 
     # dishes = ["pizza", "sauerkraut", "paella", "hamburger"]
@@ -102,7 +242,7 @@ if __name__ == '__main__':
 
     # password_check("ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR")
 
-    # collecte_team_data()
+    # collect_team_data()
 
     # team = [{'name': 'Bob', 'age': 18, 'score': 50.0},
     #         {'name': 'Tim', 'age': 17, 'score': 84.0},
@@ -112,4 +252,3 @@ if __name__ == '__main__':
 
     # l = [('V', 1), ('VI', 1), ('V', 2), ('VI', 2), ('VI', 3), ('VII', 1)]
     # classroom_stats(l)
-
