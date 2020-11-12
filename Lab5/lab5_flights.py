47f9f0cd18eccf88b96b84e894f2e47e8c54f8c2
Jelena Jovanovic <jeljov@gmail.com>
2020-11-12 14:07:57 +0100

    Completed tasks for Lab 5

--- Lab5/lab5_flights.py
+++ Lab5/lab5_flights.py
@@ -17,11 +17,43 @@
 #   that is passed to the method as its input parameter (__eq__()); two passenger objects
 #   are considered the same if they have the same name and passport number
 
+class Passenger:
+
+    def __init__(self, name, passport, business=False):
+        self.name = name
+        self.passport = passport
+        self.is_business = business
+
+    @property
+    def passport(self):
+        return self.__passport
+
+    @passport.setter
+    def passport(self, value):
+        if (isinstance(value, str)) and (len(value) == 6) and (all([ch.isdigit() for ch in value])):
+            self.__passport = value
+        elif (isinstance(value, int)) and (len(str(value)) == 6):
+            self.__passport = str(value)
+        else:
+            print("Error! Incorrect passport number! Setting passport to None")
+            self.__passport = None
 
 
+    def __str__(self):
+        passenger_str = f"passenger {self.name}, passport number: " + (self.passport if self.passport else "unknown")
+        return "Business class " + passenger_str if self.is_business else ("Economy class " + passenger_str)
 
 
-
+    def __eq__(self, other):
+        if isinstance(other, Passenger):
+            if self.passport and other.passport:
+                return (self.name == other.name) and (self.passport == other.passport)
+            else:
+                print(f"Cannot determine equality since at least one of the passengers does not have passport number")
+                return False
+        else:
+            print("The other object is not of the Passenger type")
+            return False
 
 
 
@@ -52,54 +84,130 @@
 # - methods for turning the given Flight object into an iterator (__iter__(), __next__())
 #   over the flight passengers (that is, elements of the *passengers* list)
 
+from datetime import datetime
+
+class Flight:
+
+    departure_format = "%Y-%m-%d %H:%M"
+
+    def __init__(self, fl_num, departure):
+        self.flight_num = fl_num
+        self.departure = departure
+        self.passengers = list()
 
 
+    @property
+    def departure(self):
+        return self.__departure
+
+    @departure.setter
+    def departure(self, value):
+        if isinstance(value, datetime) and (value > datetime.today()):
+            self.__departure = value
+        elif isinstance(value, str) and (datetime.strptime(value, self.departure_format) > datetime.today()):
+            self.__departure = datetime.strptime(value, self.departure_format)
+        else:
+            print(f"Error in setting the departure date and time")
+            self.__departure = None
+
+
+    def add_passenger(self, p):
+        if isinstance(p, Passenger):
+            if p not in self.passengers:
+                self.passengers.append(p)
+            else: print(str(p) + " is already in the passengers list")
+        else:
+            print("Error - received an object that is not of the Passenger type")
+
+
+    def __str__(self):
+        flight_str = f"Flight number {self.flight_num} scheduled to departure at: "
+
+        flight_str += datetime.strftime(self.departure, self.departure_format) if self.departure else "still unknown"
+
+        if len(self.passengers) == 0:
+            flight_str += "\nStill no passengers checked for the flight."
+        else:
+            flight_str += "\nPassengers:\n" + "\n".join([str(p) for p in self.passengers])
+
+        return flight_str
+
+
+    def time_to_departure(self):
+        time_diff = self.departure - datetime.today()
+        days = time_diff.days
+        secs_in_hour = 60*60
+        hours, remainder_secs = divmod(time_diff.seconds, secs_in_hour)
+        return days, hours, remainder_secs//60
+
+
+    def __iter__(self):
+        self.__iter_counter = 0
+        return self
+
+
+    def __next__(self):
+        if self.__iter_counter == len(self.passengers):
+            raise StopIteration
+        else:
+            current_passenger = self.passengers[self.__iter_counter]
+            self.__iter_counter += 1
+            return current_passenger
 
 
 
 if __name__ == '__main__':
 
-    pass
+    bob = Passenger("Bob Smith", "123456", True)
+    john = Passenger("John Smith", 987656, False)
+    jane = Passenger("Jane Smith", '987659')
 
-    # bob = Passenger("Bob Smith", "123456", True)
-    # john = Passenger("John Smith", 987656, False)
-    # jane = Passenger("Jane Smith", '987659')
-    #
-    # print(bob)
-    # print(john)
-    # print(jane)
-    #
-    # print("Checking if 'bob' and 'john' refer to the same passenger")
-    # print(bob == john)
-    # print(f"Checking if 'john' refers to the passenger 'Joe Smith', with passport number: 987656")
-    # print(john == Passenger("Joe Smith", "987656"))
+    print(bob)
+    print(john)
+    print(jane)
+
+    print("Checking if 'bob' and 'john' refer to the same passenger")
+    print(bob == john)
+    print(f"Checking if 'john' refers to the passenger 'Joe Smith', with passport number: 987656")
+    print(john == Passenger("Joe Smith", "987656"))
 
 
-    # lh1411 = Flight('LF1411', '2020-12-05 6:50')
-    # lh992 = Flight('LH992', '2020-11-25 12:20')
-    # print()
-    # print(lh1411)
-    # print()
-    # print(lh992)
-    #
-    # print()
-    #
-    # lh1411.add_passenger(bob)
-    # lh1411.add_passenger(john)
-    # lh1411.add_passenger(jane)
-    # lh1411.add_passenger(Passenger("Jimmy Smith", "987658", False))
-    # print()
-    #
-    # print(f"After adding passengers to flight {lh1411.flight_num}:")
-    # print(lh1411)
-    #
-    # print()
-    #
-    # days, hours, mins = lh1411.time_to_departure()
-    # print(f"Time till departure of the flight {lh1411.flight_num}: {days} days, {hours} hours, and {mins} minutes")
-    #
-    # print()
-    #
-    # print("Passengers on flight lh1411:")
-    # for passenger in lh1411:
-    #     print(passenger)
+    lh1411 = Flight('LF1411', '2020-12-05 6:50')
+    lh992 = Flight('LH992', '2020-11-25 12:20')
+    print()
+    print(lh1411)
+    print()
+    print(lh992)
+
+    print()
+
+    lh1411.add_passenger(bob)
+    lh1411.add_passenger(john)
+    lh1411.add_passenger(jane)
+    lh1411.add_passenger(Passenger("Jane Smith", "987659", False))
+    print()
+
+    print(f"After adding passengers to flight {lh1411.flight_num}:")
+    print(lh1411)
+
+    print()
+
+    days, hours, mins = lh1411.time_to_departure()
+    print(f"Time till departure of the flight {lh1411.flight_num}: {days} days, {hours} hours, and {mins} minutes")
+
+    print()
+    print("Passengers on flight lh1411:")
+    flight_iter = iter(lh1411)
+    try:
+        print(next(flight_iter))
+        print(next(flight_iter))
+        print(next(flight_iter))
+        print(next(flight_iter))
+    except StopIteration:
+        print("No more passengers")
+
+    print()
+
+    print("Passengers on flight lh1411 (for loop):")
+    for passenger in lh1411:
+        print(passenger)
--- Lab5/lab5_movies.py
+++ Lab5/lab5_movies.py
@@ -12,9 +12,46 @@
 #
 # - a method that returns a string representation of a MovieReview object (__str__())
 
+from datetime import datetime
 
+class MovieReview:
 
+    def __init__(self, rating, comment=""):
+        self.rating = rating
+        self.comment = comment
+        self.timestamp = datetime.today()
 
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        if isinstance(value, int) and 1 <= value <= 5:
+            self.__rating = value
+        elif isinstance(value, str) and (len(value) == 1) and (value in '12345'):
+            self.__rating = int(value)
+        else:
+            print(f"Invalid value ({value}) passed for movie rating")
+            self.__rating = None
+
+    @property
+    def comment(self):
+        return self.__comment if self.__comment else ""
+
+    @comment.setter
+    def comment(self, value):
+        if isinstance(value, str):
+            self.__comment = value
+        else:
+            print(f"Error! String value expected, received {type(value)} instead. Coercing the input to a string")
+            self.__comment = str(value)
+
+    def __str__(self):
+        s = f"{self.rating} stars; " if self.rating else "Rating not available; "
+        s += f"comment: '{self.comment}'" if self.comment else "no comment left"
+        s += f"(received {datetime.strftime(self.timestamp, '%b %d, %Y %H:%M')})"
+        return s
 
 
 # Create the Movie class with the following methods:
@@ -41,45 +78,97 @@
 #   over the movie reviews (that is, elements of the *reviews* list)
 #
 
+class Movie:
+
+    def __init__(self, title, year, director=None):
+        self.title = title
+        self.year = year
+        self.director = director
+        self.reviews = list()
 
 
+    def __str__(self):
+        movie_str = f"Movie '{self.title}' from {self.year}"
+        movie_str += f" directed by {self.director}" if self.director else " (director unknown)"
+        if len(self.reviews) > 0:
+            movie_str += "\nReviews:\n" + "\n".join([str(mr) for mr in self.reviews])
+        else:
+            movie_str += ", no reviews yet"
+        return movie_str
+
+
+    def add_review(self, review):
+        if isinstance(review, MovieReview):
+            time_diff = datetime.today() - review.timestamp
+            time_diff_sec = time_diff.total_seconds()
+            secs_in_year = 365*24*60*60
+            time_diff_year = time_diff_sec // secs_in_year # integer division
+            if time_diff_year < 1:
+                self.reviews.append(review)
+            else:
+                print("An outdated review")
+        else:
+            print("Not an object of MovieReview class; cannot be added")
+
+
+    def __eq__(self, other):
+        if isinstance(other, Movie):
+            if self.director and other.director:
+                return (self.title == other.title) and (self.director == other.director)
+            else:
+                print("Director(s) unknown; checking for equality based on the title-year pair")
+                return (self.title == other.title) and (self.year == other.year)
+        else:
+            print("The other object is not a Movie")
+            return False
+
+
+    def __iter__(self):
+        self.__review_counter = 0
+        return self
+
+    def __next__(self):
+        if self.__review_counter == len(self.reviews):
+            raise StopIteration
+
+        current_review = self.reviews[self.__review_counter]
+        self.__review_counter += 1
+        return current_review
 
 
 if __name__ == '__main__':
 
-    pass
-
-    # mr_1 = MovieReview(5, "Superb!")
-    # mr_2 = MovieReview(5, "The best ever!")
-    # mr_3 = MovieReview(3, "Expected more...")
+    mr_1 = MovieReview(5, "Superb!")
+    mr_2 = MovieReview(5, "The best ever!")
+    mr_3 = MovieReview(3, "Expected more...")
 
     # print(mr_1)
     # print(mr_2)
     # print(mr_3)
 
-    # godfather = Movie("The Godfather", year=1972, director="Francis Ford Coppola")
-    # print(godfather)
-    #
-    # print()
-    #
-    # godfather_2 = Movie("The Godfather: part II", 1974, "Francis Ford Coppola")
-    # print(godfather_2)
-    #
-    # print()
-    #
-    # if godfather == godfather_2:
-    #     print("No difference observed!")
-    # else:
-    #     print("Different movies!")
-    #
-    # print()
-    #
-    # for mr in (mr_1, mr_2, mr_3):
-    #     godfather_2.add_review(mr)
-    #
-    # print("Printing movie data after adding reviews")
-    # print(godfather_2)
-    #
-    # print("\nReviews of the Godfather 2 movie:")
-    # for review in godfather_2:
-    #     print(review)
+    godfather = Movie("The Godfather", year=1972, director="Francis Ford Coppola")
+    print(godfather)
+
+    print()
+
+    godfather_2 = Movie("The Godfather: part II", 1974, "Francis Ford Coppola")
+    print(godfather_2)
+
+    print()
+
+    if godfather == godfather_2:
+        print("No difference observed!")
+    else:
+        print("Different movies!")
+
+    print()
+
+    for mr in (mr_1, mr_2, mr_3):
+        godfather_2.add_review(mr)
+
+    print("Printing movie data after adding reviews")
+    print(godfather_2)
+
+    print("\nReviews for the Godfather 2 movie:")
+    for review in godfather_2:
+        print(review)
